75f02ccbef028533863a3f89955c9ce3cb33b9e1
ketsuog0d <geniusgpt707@gmail.com>
2025-01-29 10:37:27 +0500

    first

--- news_site/urls.py
+++ news_site/urls.py
@@ -21,7 +21,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('index.urls'))
+    path('', include('index.urls')),
+    path('accounts/', include('django.contrib.auth.urls'))
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
